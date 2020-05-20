bef629f94305ff47a2676560a88ca041900a1e98
tegarsweefer <tegar.s@weefer.co.id>
2020-05-20 11:37:21 +0700

    berhasil membaca file .xlsx dan .xls, tapi tidak bisa membaca csv file

---
+++ Controllers/ReadController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using ExcelDataReader;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LearnExcelDataReader.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReadController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult Get()
+        {
+            string text = "";
+            System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
+            using (var stream = System.IO.File.Open("Files/Book1.xlsx", FileMode.Open, FileAccess.Read))
+            {
+                using (var reader = ExcelReaderFactory.CreateReader(stream))
+                {
+                    do
+                    {
+                        while (reader.Read()) //Each ROW
+                        {
+                            for (int column = 0; column < reader.FieldCount; column++)
+                            {
+                                //Console.WriteLine(reader.GetString(column));//Will blow up if the value is decimal etc. 
+                                Console.WriteLine(reader.GetValue(column));//Get Value returns object
+                                text+=reader.GetValue(column)+", ";
+                            }
+                            text +="<br/>";
+                        }
+                    } while (reader.NextResult()); //Move to NEXT SHEET
+
+                }
+            }
+            return Ok(text);
+        }
+    }
+}
