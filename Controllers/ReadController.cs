9fcbd6a141f872b0c99890cfd696d3564275e6f0
tegarsweefer <tegar.s@weefer.co.id>
2020-05-20 11:57:58 +0700

    read excel from upload file (stream)

--- Controllers/ReadController.cs
+++ Controllers/ReadController.cs
@@ -70,5 +70,38 @@ namespace LearnExcelDataReader.Controllers
             }
             return Ok(products);
         }
+        [HttpPost("Upload")]
+        [RequestSizeLimit(8000000)]
+        public ActionResult Upload(IFormFile file)
+        {
+            string[] permittedExtensions = { ".xlsx",".xls" };
+            var ext = Path.GetExtension(file.FileName).ToLowerInvariant();
+            if (string.IsNullOrEmpty(ext) || !permittedExtensions.Contains(ext))
+                return BadRequest("Only Allowed Excel File");
+
+
+            List<Product> products = new List<Product>();
+            bool first = true;
+            System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
+            using (var reader = ExcelReaderFactory.CreateReader(file.OpenReadStream()))
+            {
+                while (reader.Read())
+                {
+                    if (first)
+                        first = false;
+                    else
+                    {
+                        var product = new Product()
+                        {
+                            Name = reader.GetValue(0).ToString(),
+                            Price = reader.GetValue(1).ToString(),
+                            Description = reader.GetValue(2).ToString()
+                        };
+                        products.Add(product);
+                    }
+                }
+            }
+            return Ok(products);
+        }
     }
 }
